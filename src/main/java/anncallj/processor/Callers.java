b374c712c06755ec0d22085f265b1507499085b3
fits <261649+fits@users.noreply.github.com>
2014-08-29 23:04:10 +0900

    簡易呼び出し用のメソッド追加

--- src/main/java/anncallj/processor/Callers.java
+++ src/main/java/anncallj/processor/Callers.java
@@ -1,11 +1,27 @@
 package anncallj.processor;
 
 public class Callers {
+	public static <S> Caller<S> caller(CallParser<S> parser, Class<?> cls) {
+		return new BasicCaller<>(parser, cls);
+	}
+
+	public static <S> Caller<S> caller(CallParser<S> parser, Object obj) {
+		return new BasicCaller<>(parser, obj);
+	}
+
 	public static Caller<String> regexCaller(Class<?> cls) {
 		return new BasicCaller<>(new RegexCallParser(), cls);
 	}
 
 	public static Caller<String> regexCaller(Object obj) {
 		return new BasicCaller<>(new RegexCallParser(), obj);
+	}
+
+	public static Caller<Object> ognlCaller(Class<?> cls) {
+		return new BasicCaller<>(new OgnlCallParser(), cls);
+	}
+
+	public static Caller<Object> ognlCaller(Object obj) {
+		return new BasicCaller<>(new OgnlCallParser(), obj);
 	}
 }
